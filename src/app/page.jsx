070ae7471fbf914a00f2309836efa2dd2cbccc80
Omsurvase24 <omsurvase24@gmail.com>
2024-07-16 18:26:21 +0530

    homepage design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,26 @@
+import styles from "./home.module.css"
+import Image from "next/image";
+
 const Home = () => {
-  return <div>Homepage</div>;
+  return <div className={styles.container}>
+    <div className={styles.textContainer}>
+      <h1 className={styles.title}>Creative Thoughts Agency.</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut
+        earum nesciunt, quidem dolore non mollitia porro id, inventore neque provident repudiandae hic, delectus quia optio quam reiciendis aperiam est sapiente.
+      </p>
+      <div className={styles.buttons}>
+        <button className={styles.button}>Learn More</button>
+        <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+        <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+      </div>
+    </div>
+    <div className={styles.imgContainer}>
+      <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+    </div>
+  </div>;
 };
 
 export default Home;
