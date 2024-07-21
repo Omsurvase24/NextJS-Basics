75d81d5c6423654199f4ef1f68d962c8839fac10
Omsurvase24 <omsurvase24@gmail.com>
2024-07-21 20:05:30 +0530

    data fetching and caching

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,16 +1,29 @@
 import styles from "./singlePost.module.css"
 import Image from "next/image"
 
-const SinglePostPage = ({ params }) => {
+const getData = async (slug) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
 
-    console.log(params)
+    if (!res.ok) {
+        throw new Error("Something went wrong");
+    }
+    return res.json();
+}
+
+const SinglePostPage = async ({ params }) => {
+
+
+    const { slug } = params;
+
+    const post = await getData(slug);
+
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
                 <Image src="https://images.pexels.com/photos/26078432/pexels-photo-26078432/free-photo-of-altes-land.jpeg" alt="" fill className={styles.img} />
             </div>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>Title</h1>
+                <h1 className={styles.title}>{post.title}</h1>
                 <div className={styles.detail}>
                     <Image className={styles.avatar} src="https://images.pexels.com/photos/26078432/pexels-photo-26078432/free-photo-of-altes-land.jpeg" alt="" width={50} height={50} />
                     <div className={styles.detailText}>
@@ -23,7 +36,7 @@ const SinglePostPage = ({ params }) => {
                     </div>
                 </div>
                 <div className={styles.content}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit natus saepe quod, eaque quo. Quam, rerum odio consequatur animi vitae eos. Quasi quod obcaecati sint aliquid deleniti nobis consequuntur.
+                    {post.body}
                 </div>
             </div>
         </div>
